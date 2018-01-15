b97eaa186ceca8a0e1a71e1971f48d054fd83957
theRealSheng <sheng.huchai@gmail.com>
2018-01-15 12:09:19 +0100

    Soldier prototype done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,5 +1,20 @@
 // Soldier
-function Soldier() {}
+function Soldier(health, strength) {
+    this.name = name;
+    this.health = health;
+    this.strength = strength;
+}
+
+Soldier.prototype.attack = function () {
+        return this.strength;
+};
+
+Soldier.prototype.receiveDamage = function (damage) {
+        this.health = this.health - damage;
+};
+
+
+
 
 // Viking
 function Viking() {}
